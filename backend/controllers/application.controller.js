0719dc94f505791fc51bb07cfc2bb7ece834e25a
HarshadBharade <bharadeharshad106@gmail.com>
2025-06-28 11:12:37 +0530

    application models and routes created

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -74,3 +74,61 @@ export const getAppliedJobs = async(req,res)=>{
     console.log(error);
   }
 }
+
+export const getApplicants = async(req,res)=>{
+  try {
+    
+    const jobId = req.params.id;
+    const job = await Job.findById(jobId).populate({
+      path:'applications',
+      option:{sort:{createdAt: -1}},
+      populate:{
+        path:'applicant',
+      }
+    });
+    if(!job){
+      return res.status(404).json({
+        message:"Job not found",
+        success:false
+        });
+    }
+    return res.status(200).json({
+      job,
+      success:true
+      })
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+export const updateStatus = async(req,res)=>{
+  try {
+    const {status} = req.body;
+    const applicationId = req.params.id;
+    if(!status){
+      return res.status(404).json({
+        message:"status is required",
+        success:false
+        })
+    };
+    // find the application by application Id
+    const application = await Application.findOne({_id:applicationId});
+    if(!application){
+      return res.status(400).json({
+        message:"Application not found",
+        success:false
+      })
+    };
+
+    // update status
+    application.status = status.LowerCase();
+    await application.save();
+
+    return res.status(200).json({
+      message:" status  updated successfully",
+      success:true
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
