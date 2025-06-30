ca741d28c7c665e4e67b69d41c60b602268d77f7
HarshadBharade <bharadeharshad106@gmail.com>
2025-06-30 13:54:21 +0530

    Navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,28 @@
-import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
 import Navbar from './components/shared/navbar'
+
+const appRouter = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/',
+    element:<Home/>
+  }
+])
 
 function App(){
  return (
